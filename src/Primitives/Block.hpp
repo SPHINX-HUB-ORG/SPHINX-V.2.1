1fd48b676844c3b428da883710ec8008e18e67eb
Kusuma <thekoesoemo@gmail.com>
2023-09-01 03:14:24 +0700

    improvement

--- src/Primitives/Block.hpp
+++ src/Primitives/Block.hpp
@@ -6,84 +6,151 @@
 #ifndef SPHINXBLOCK_HPP
 #define SPHINXBLOCK_HPP
 
-#include <cstdint>
-#include <iostream>
-#include <unordered_map>
-
-#include <stdexcept>
-#include <fstream> 
-#include <iostream>
 #include <ctime>
 #include <string>
-#include <vector>
-#include <array>
+#include <chrono>
 
-#include "SphinxJS/jsonrpcpp/include/json.hpp"
 #include "Transaction.hpp"
-#include "Hash.hpp"
-
+#include <SphinxJS/jsonrpcpp/include/json.hpp>
 
 namespace SPHINXBlock {
-    class Block {
-    public:
-        struct BlockHeader {
-            uint32_t version;
-            std::string HashPrevBlock;
-            std::string HashMerkleRoot;
-            std::string signature;
-            uint32_t blockHeight;
-            std::time_t timestamp;
-            uint32_t nonce;
-            uint32_t difficulty;
-        };
 
-    private:
-        std::string HashPrevBlock_;
-        std::string HashMerkleRoot_;
-        std::string signature_;
-        uint32_t blockHeight_;
-        std::time_t timestamp_;
-        uint32_t nonce_;
-        uint32_t difficulty_;
-        uint32_t version_;
-        std::vector<std::string> transactions_;
-        SPHINXChain::Chain* blockchain_;
-        const std::vector<std::string>& checkpointBlocks_;
-        std::string storedMerkleRoot_;
-        std::string storedSignature_;
+    struct BlockHeader {
+        uint32_t version;
+        std::string hashPrevBlock;
+        std::string hashMerkleRoot;
+        std::string signature;
+        uint32_t blockHeight;
+        std::time_t timestamp;
+        uint32_t nonce;
+        uint32_t difficulty;
 
-    public:
-        static const uint32_t MAX_BLOCK_SIZE = 1000;
-        static const uint32_t MAX_TIMESTAMP_OFFSET = 600;
-
-        int getIndex() const;
-        BlockHeader getBlockHeader() const;
-
-        Block(const std::string& prevBlockHash, const std::string& timestamp, const std::string& nonce, const std::vector<Transaction>& transactions, const std::string& version, const std::string& previousHash = "", int index = 0);
-
-        BlockHeader getBlockHeader() const {
-            BlockHeader header;
-            header.version = version_;
-            header.HashPrevBlock = HashPrevBlock_;
-            header.HashMerkleRoot = HashMerkleRoot_;
-            header.signature = signature_;
-            header.blockHeight = blockHeight_;
-            header.timestamp = timestamp_;
-            header.nonce = nonce_;
-            header.difficulty = difficulty_;
-            return header;
+        // Constructor
+        BlockHeader(){
+            SetNull();
         }
 
-        static nlohmann::json toJson(const BlockHeader& header);
-        static BlockHeader fromJson(const nlohmann::json& headerJson);
-        static std::string toString(const BlockHeader& header);
+        // Serialization methods using nlohmann::json
+        void to_json(nlohmann::json& j) const {
+            j = nlohmann::json{{"version", version}, {"hashPrevBlock", hashPrevBlock}, {"hashMerkleRoot", hashMerkleRoot}, {"timestamp", timestamp}, {"difficulty", difficulty}, {"nonce", nonce}};
+        }
 
-        bool save(const std::string& filename) const;
-        static Block load(const std::string& filename);
-        bool saveToDatabase(SPHINXDb::DistributedDb& distributedDb) const;
-        static Block loadFromDatabase(const std::string& blockId, SPHINXDb::DistributedDb& distributedDb);
+        void from_json(const nlohmann::json& j) {
+            version = j["version"];
+            hashPrevBlock = j["hashPrevBlock"];
+            hashMerkleRoot = j["hashMerkleRoot"];
+            timestamp = j["timestamp"];
+            difficulty = j["difficulty"];
+            nonce = j["nonce"];
+        }
 
-        std::string getStoredMerkleRoot() const;
-        std::string getStoredSignature() const;
+        // Set all members to null values
+        void SetNull(){
+            version = 0;
+            hashPrevBlock.clear();
+            hashMerkleRoot.clear();
+            timestamp = 0;
+            difficulty = 0;
+            nonce = 0;
+        }
+
+        // Check if the block is null
+        bool IsNull() const{
+            return (difficulty == 0);
+        }
+
+        // Get the hash of the block
+        SPHINXHash::SPHINX_256 GetHash() const;
+
+        // Get timestamp as NodeSeconds
+        NodeSeconds Timestamp() const{
+            return NodeSeconds{std::chrono::seconds{timestamp}};
+        }
+
+        // Get block timestamp as int64_t
+        int64_t GetBlockTimestamp() const{
+            return static_cast<int64_t>(timestamp);
+        }
     };
+
+    struct Block : public BlockHeader {
+        std::vector<CTransactionRef> vtx;
+        mutable bool fChecked;
+
+        // Default constructor
+        Block(){
+            SetNull();
+        }
+
+        // Constructor with header
+        Block(const BlockHeader &header){
+            SetNull();
+            *(static_cast<BlockHeader*>(this)) = header;
+        }
+
+        // Serialization methods using nlohmann::json
+        void to_json(nlohmann::json& j) const {
+            to_json(static_cast<const BlockHeader&>(*this)); // Call parent's to_json method
+            j["vtx"] = vtx;
+        }
+
+        void from_json(const nlohmann::json& j) {
+            from_json(j); // Call parent's from_json method
+            vtx = j["vtx"];
+        }
+
+        // Set all members to null values
+        void SetNull(){
+            BlockHeader::SetNull();
+            vtx.clear();
+            fChecked = false;
+        }
+
+        // Get the block header
+        BlockHeader GetBlockHeader() const{
+            BlockHeader block;
+            block.version           = version;
+            block.hashPrevBlock     = hashPrevBlock;
+            block.hashMerkleRoot    = hashMerkleRoot;
+            block.timestamp         = timestamp;
+            block.difficulty        = difficulty;
+            block.nonce             = nonce;
+            return block;
+        }
+
+        // Convert the block to a string representation
+        std::string ToString() const;
+    };
+
+    struct BlockLocator {
+        std::vector<SPHINXHash::SPHINX_256> vHave;
+
+        // Default constructor
+        BlockLocator() {}
+
+        // Constructor with vector
+        explicit BlockLocator(std::vector<SPHINXHash::SPHINX_256>&& have) : vHave(std::move(have)) {}
+
+        // Serialization methods using nlohmann::json
+        void to_json(nlohmann::json& j) const {
+            j["vHave"] = vHave;
+        }
+
+        void from_json(const nlohmann::json& j) {
+            vHave = j["vHave"];
+        }
+
+        // Set all members to null values
+        void SetNull(){
+            vHave.clear();
+        }
+
+        // Check if the block locator is null
+        bool IsNull() const{
+            return vHave.empty();
+        }
+    };
+
 } // namespace SPHINXBlock
+
+
